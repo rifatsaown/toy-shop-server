9a61fb63d5b63a3ea290f9a9bfc3a91b9ca54425
Md Rifat Hossen <rifatsaown0@gmail.com>
2023-06-10 08:05:53 +0600

    Server Start

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const app = express();
+require('dotenv').config();
 const cors = require('cors');
 const port = process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.USER_ID}:${process.env.USER_PASS}@cluster0.meus3dj.mongodb.net/?retryWrites=true&w=majority`;
+
 // use middleware
 app.use(cors());
 app.use(express.json());
@@ -11,6 +15,31 @@ app.get('/', (req, res) => {
 });
 
 //MongoDB connection
+
+
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
+}
+run().catch(console.dir);
 
 
 
